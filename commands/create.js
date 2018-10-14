e6f43bafa9866250a4e74a473f2ccd992f4f66ba
Waves <creepykid1101@gmail.com>
2018-10-14 22:38:39 +0000

    Update create.js

--- commands/create.js
+++ commands/create.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const settings = require('./jsons/settings.json');
 
 exports.run = (bot, message, args, tools) => {
-
+if(!message.author.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have permission to create a faction (Manage Roles)');
 if(!args) return message.channel.send('Please provide a name for the faction.');
 try{
 message.guild.createRole({
