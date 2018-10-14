4e77186c3159725d02fd7beea4348b8ae7a15b7f
Waves <creepykid1101@gmail.com>
2018-10-14 23:05:24 +0000

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -5,10 +5,9 @@ exports.run = async (bot, message, args, tools) => {
 let facs = message.guild.roles.filter(role => role.name.startsWith('⚔'));
 let str = args.join(" ");
 let role = facs.find(role => role.name.slice(2).toLowerCase() === str.toLowerCase());
-if(facs.size < 0) return message.channel.send('Please leave the faction you\'re in first.');
+if(message.member.roles.filter(r => r.name.startsWith('⚔')).size < 0) return message.channel.send('Please leave the faction you\'re in first.');
 
 try{
-await message.member.removeRoles(facs);
 await message.member.addRole(role);
 message.channel.send(`You have joined ${role}.`);
 } catch(e) {
