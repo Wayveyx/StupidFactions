644653ceafb874c4d639719d206bb7e42d2067e1
Waves <creepykid1101@gmail.com>
2018-10-15 01:43:11 +0000

    Update create.js

--- commands/create.js
+++ commands/create.js
@@ -5,7 +5,7 @@ const no = ["fuck", "damn", "shit", "cunt", "ass", "gay", "lesbian", "bitch", "c
 exports.run = (bot, message, args, tools) => { 
 if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have permission to create a faction. (Manage Roles)');
 if(no.some(word => message.content.includes(word))) return message.channel.send('How dare u try to name it that');
-if(!message.content) return message.channel.send('Please provide a name for the faction.');
+if(!args) return message.channel.send('Please provide a name for the faction.');
 try{
 message.guild.createRole({
 name:`⚔ ${args.join(" ")}`
