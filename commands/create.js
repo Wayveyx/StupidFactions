a9afc7f2a314982f87df65fcf545a03068b55589
Waves <creepykid1101@gmail.com>
2018-10-14 20:20:26 -0500

    added word filter

--- commands/create.js
+++ commands/create.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const settings = require('./jsons/settings.json');
+const no = ["fuck", "damn", "shit", "cunt", "ass", "gay", "lesbian", "bitch", "cock", "pussy", "clit", "penis", "nigger", "nigga", "nig", "niggah", "nigg", "vagina", "porn", "hentai", "shemale"];
 
-exports.run = (bot, message, args, tools) => {
-if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have permission to create a faction (Manage Roles)');
+exports.run = (bot, message, args, tools) => { 
+if(!message.member.hasPermission('MANAGE_ROLES') return message.channel.send('You do not have permission to create a faction. (Manage Roles)');
+if(no.some(word => message.content.includes(word))) return message.channel.send('How dare u try to name it that');
 if(!args) return message.channel.send('Please provide a name for the faction.');
 try{
 message.guild.createRole({
