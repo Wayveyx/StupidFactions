54f78a9104e4867cfbbb1e0469ef8e427756785a
Waves <creepykid1101@gmail.com>
2018-10-24 13:20:16 -0500

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,6 +1,16 @@
 const Discord = requre('discord.js');
 const settings = require('./jsons/settings.json')
 
-exports.run = (bot, message, args, tools) +>=> {
+exports.run = async (bot, message, args, tools) +>=> {
+if(!message.member.hasPermission('MANAGE_ROLES')) return;
+let facs = message.guild.roles.filter(role => role.name.startsWith('⚔'));
+if(facs.size < 1) return message.channel.send(new Discord.RichEmbed()
+.setTitle('No Factions')
+.setDescription('This server has no factions.')
+.setColor([255, 0, 0]));
+let str = args.join(" ");
+let role = facs.find(role => role.name.slice(1).toLowerCase() === str.toLowerCase());
 
+await message.guild.deleteRole(role)
+message.channel.send('Deleted the faction.');
 }
