bed336247fac55567ab2e8a83365118578ba10da
Waves <creepykid1101@gmail.com>
2018-10-25 13:11:09 -0500

    Update sfac.js

--- sfac.js
+++ sfac.js
@@ -4,23 +4,23 @@ const bot = new Discord.Client();
 const pfix = 'SF;';
 
 bot.on("ready", () => {
-bot.user.setActivity(`Factions | sf;help`);
-console.log(`Factions?`);
+  bot.user.setActivity(`Factions | sf;help`);
+  console.log(`Factions?`);
 });
 
 bot.on("message", message => {
-let msg = message.content.toUpperCase();
-let args = message.content.slice(pfix.length).trim().split(' ');
-let cmd = args.shift().toLowerCase();
-if(!msg.startsWith(pfix)) return;
-if(msg === pfix + 'TEST') return message.channel.send('Seems to be working :thinking:');
+  let msg = message.content.toUpperCase();
+  let args = message.content.slice(pfix.length).trim().split(' ');
+  let cmd = args.shift().toLowerCase();
+  if(!msg.startsWith(pfix)) return;
+  if(msg === pfix + 'TEST') return message.channel.send('Seems to be working :thinking:');
 
-try{
-let commandFile = require(`./commands/${cmd}.js`);
-commandFile.run(bot, message, args);
-} catch(e) {
-message.channel.send(e.message);
-}
+  try{
+    let commandFile = require(`./commands/${cmd}.js`);
+    commandFile.run(bot, message, args);
+  } catch(e) {
+    message.channel.send(e.message);
+  }
 
 });
 
